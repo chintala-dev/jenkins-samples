69e20070c479d0a5c3635445f6ba0ec32e74c67c
chintala-dev <44329145+chintala-dev@users.noreply.github.com>
2021-05-17 00:18:49 -0400

    Create secondjob.groovy

---
+++ DSL/jobs/secondjob.groovy
@@ -0,0 +1,16 @@
+job('Second DSL Job') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+//    triggers {
+//        scm('H/5 * * * *')
+//    }
+    steps {
+        remoteShell('appadmin@10.200.1.4:22') {
+            command('echo $hostName')
+            command('ls -lart ansible_projects')
+        }
+}
