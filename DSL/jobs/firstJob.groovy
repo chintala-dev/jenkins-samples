58f435b1fe7c03e899aac3254d0d212857c2a407
chintala-dev <44329145+chintala-dev@users.noreply.github.com>
2021-05-17 00:04:39 -0400

    Create firstJob.groovy
    This is used to create a Jenkins job using job definition from Git.

---
+++ DSL/jobs/firstJob.groovy
@@ -0,0 +1,11 @@
+job('Hello World Job') {
+    scm {
+        git('git://github.com/chintala-dev/jenkins-samples.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Git User')
+            node / gitConfigEmail('user@user.com')
+        }
+    }
+    steps {
+        shell("Hello Wrold")
+    }
+}
