30d6cb67d226c255d2647c725c0a1d333049ddfb
chintala-dev <44329145+chintala-dev@users.noreply.github.com>
2021-05-17 00:14:58 -0400

    Update firstJob.groovy

--- DSL/jobs/firstJob.groovy
+++ DSL/jobs/firstJob.groovy
@@ -1,11 +1,5 @@
 job('First DSL Job') {
-    scm {
-        git('git://github.com/chintala-dev/jenkins-samples.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('Git User')
-            node / gitConfigEmail('user@user.com')
-        }
-    }
     steps {
-        shell("echo 'Hello Wrold'")
+        shell("echo 'Hello World'")
     }
 }
